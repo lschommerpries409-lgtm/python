658c53028348fee93884a876c74b0fd0d9c42386
lschommerpries409-lgtm <lschommerpries409@phm.k12.in.us>
2026-09-21 12:19:50 -0400

    Final Submission with Comments
    I added comments to my code test file.

--- G3TiCS/pandas_tut.py
+++ G3TiCS/pandas_tut.py
@@ -3,7 +3,7 @@
 import pandas as pd 
 #import G3TiCS/pennData500.csv 
 
-scores = pd.DataFrame(
+scores = pd.DataFrame( #Sets up a basic data frame 
     [
         [85, 91, 78, 92],
         [88, 76, 95, 89],
@@ -15,15 +15,13 @@ scores = pd.DataFrame(
 )
 
 print(scores) 
-print("This is a test file for GitHub documentation purposes.  ")
-print("this is a new line  .") 
 
-print("    ----     ")
+print("    ----     ") #This is a divider to make the printed information more seperatied 
 
-data = pd.read_csv("G3TiCS/pennData500.csv") 
+data = pd.read_csv("G3TiCS/pennData500.csv") #Imports the cvs file, converts it to a dataframe, and names it data 
 print(" ") 
 
-print(data.head()) 
+print(data.head()) #Prints the head and tail of the data 
 
 print("    ----     ")
 
@@ -31,22 +29,60 @@ print(data.tail())
 
 print("    ----     ")
 
-print(data.shape) 
+print(data.shape) #Prints the shape of the data 
 
 print("    ----     ")
 
-print(data.columns) 
+print(data.columns) #Prints the column labels 
 
 print("    ----     ")
 
-print(data.dtypes) 
+print(data.dtypes) #Prints the column labels and what type of data they contain 
 
 print("    ----     ") 
 
-print(data.describe()) 
+print(data.describe()) #Prints some basic information about the numeric data, such as average and minimum 
 
 print("    ----     ") 
 
+print(data["Age"].mean()) #Manually prints some basic information about Age 
+print(data["Age"].median()) 
+print(data["Age"].min()) 
+print(data["Age"].max()) 
+
+print("    ----     ") 
+
+print(data["Year"]) #Prints (the head and tail) of just the Year column 
+
+print("    ----     ") 
+
+print(data[["Name", "Pathway"]]) #Prints just the Name and Pathway columns 
+
+print("    ----     ") 
+
+filtered_data = data[data["Credits Completed"] > 50] #Filters the data and prints just the rows where the student has completed more than 50 credits 
+
+print(filtered_data) 
+
+print("    ----     ") 
+
+filtered_data = data[data["Pathway"] == "Computer Science"] #Filters and prints just the rows where the student is in the CS Pathway 
+
+print(filtered_data) 
+
+print("    ----     ") 
+
+filtered_data = data[data["GPA"] > 4.0] #Filters the data so that it's just the students who have a GPA of over 4.0 
+print(filtered_data[["Name", "Year"]]) #Prints the Name and Year of the students from the filtered data 
+print(filtered_data.shape) #Prints the shape of the filtered data 
+
+print("    ----     ") 
+
+print(data.shape) #Prints the shape of the data set 
+
+print("    ----     ") 
+
+print(data["Age"]) #Prints the age column and then various basic information about the data 
 print(data["Age"].mean()) 
 print(data["Age"].median()) 
 print(data["Age"].min()) 
@@ -54,54 +90,16 @@ print(data["Age"].max())
 
 print("    ----     ") 
 
-print(data["Year"]) 
-
-print("    ----     ") 
-
-print(data[["Name", "Pathway"]]) 
-
-print("    ----     ") 
-
-filtered_data = data[data["Credits Completed"] > 50] 
-
-print(filtered_data) 
-
-print("    ----     ") 
-
-filtered_data = data[data["Pathway"] == "Computer Science"]
-
-print(filtered_data) 
-
-print("    ----     ") 
-
-filtered_data = data[data["GPA"] > 4.0] 
-print(filtered_data[["Name", "Year"]]) 
+filtered_data = data[data["Year"] == "Sophomore"] #Filters the data for just Sophomores and prints the data shape 
 print(filtered_data.shape) 
 
 print("    ----     ") 
 
-print(data.shape) 
+print(data[["Age", "Credits Completed"]]) #Prints just the age and credits completed columns 
 
 print("    ----     ") 
 
-print(data["Age"]) 
-print(data["Age"].mean()) 
-print(data["Age"].median()) 
-print(data["Age"].min()) 
-print(data["Age"].max()) 
-
-print("    ----     ") 
-
-filtered_data = data[data["Year"] == "Sophomore"] 
-print(filtered_data.shape) 
-
-print("    ----     ") 
-
-print(data[["Age", "Credits Completed"]]) 
-
-print("    ----     ") 
-
-CS_filter = data[data["Pathway"] == "Computer Science"] 
+CS_filter = data[data["Pathway"] == "Computer Science"] #Filters the data for CS pathway students and prints the data shape 
 print(CS_filter.shape) 
-business_filter = data[data["Pathway"] == "Business"] 
+business_filter = data[data["Pathway"] == "Business"] #Filters the buisness pathway students and prints the shape of the data 
 print(business_filter.shape) 
